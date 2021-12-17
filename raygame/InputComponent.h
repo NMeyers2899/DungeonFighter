a4aeff687cc8851fcf95f9b7db03f9c41838bcc0
nicholasMeyers <s218019@student.aie.edu>
2021-12-17 09:36:44 -0600

    Made Functions that Return Whether or not the Action Keys were Pressed

--- raygame/InputComponent.h
+++ raygame/InputComponent.h
@@ -8,7 +8,17 @@ class InputComponent : public Component
 public:
 	InputComponent(const char* name = "InputComponent") : Component::Component(name) {}
 
+	// Gets the movement of the player.
 	MathLibrary::Vector2 getMoveAxis();
+
+	// Possible actions regarding the action keys.
+	bool getAction1Pressed();
+	bool getAction3Pressed();
+	bool getAction4Pressed();
+
+	bool getAction1Down();
+	bool getAction3Down();
+	bool getAction4Down();
 
 private:
 	// Key for the player putting up their shield.
@@ -17,7 +27,7 @@ private:
 	// Key for the player swining their sword.
 	RAYLIB_H::KeyboardKey m_action3Key = RAYLIB_H::KeyboardKey::KEY_E;
 	// Key for the player firing their bow.
-	RAYLIB_H::KeyboardKey m_action3Key = RAYLIB_H::KeyboardKey::KEY_F;
+	RAYLIB_H::KeyboardKey m_action4Key = RAYLIB_H::KeyboardKey::KEY_F;
 	RAYLIB_H::KeyboardKey m_submitKey = RAYLIB_H::KeyboardKey::KEY_ENTER;
 	RAYLIB_H::KeyboardKey m_cancelKey = RAYLIB_H::KeyboardKey::KEY_BACKSPACE;
 };
